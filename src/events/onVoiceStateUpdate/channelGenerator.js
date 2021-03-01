2a825ac81fc2381b75a0355f3039943dc877cd09
Dean Leszman <dean.leszman@gmail.com>
2021-03-01 18:31:39 +0000

    Updated packages. Fixed channel generator

--- src/events/onVoiceStateUpdate/channelGenerator.js
+++ src/events/onVoiceStateUpdate/channelGenerator.js
@@ -1,11 +1,11 @@
 const channels = {};
-channels["757629611919147251"] = true; // Lobby
 channels["704340805346787379"] = true; // A Quiet Place
-channels["711310684293431402"] = true; // Do Not Disturb / AFK
+channels["757629611919147251"] = true; // Channel Creator
 channels["704339593637396481"] = true; // Big Boi Lounge
 
 /* Create a new voice channel when somebody joins the lobby voice channel */
 module.exports = function (oldState, newState) {
+    
     if (oldState.channel) {
         if (!oldState.channel.members.first()) {
             if (!channels[oldState.channel.id]) {
@@ -16,9 +16,10 @@ module.exports = function (oldState, newState) {
 
     if (newState.channel) {
         if (newState.channel.name === "Channel Creator") {
+            const voiceCategoryChannel = newState.guild.channels.cache.find(x => x.type === 'category' && x.name === 'Voice');
             newState.guild.channels.create(`🎮 ${newState.member.displayName}'s Room `, {
                 type: 'voice',
-                parent: newState.guild.channels.cache.find(x => x.type === 'category' && x.name === 'Voice')
+                parent: voiceCategoryChannel
             })
             .then((channel) => {
                 newState.setChannel(channel);
