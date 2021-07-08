fbcdfe7292eeed37214eaf79f29364b1efc74f8f
Dean Leszman <dean.leszman@gmail.com>
2021-07-08 09:36:08 +0100

    0.12.11 ammo

--- src/commands/games/escapefromtarkov.js
+++ src/commands/games/escapefromtarkov.js
@@ -5,102 +5,102 @@ const ammoTypes = [];
 
 ammoTypes["12 Gauge"] = {
     name: '12 Gauge',
-    value: 'Budget Ammo: *12/70 8.5 mm "Magnum" Buckshot*\nBest Ammo: *12/70 AP-20 Slug*\nBest Weapon: *Saiga 12ga ver. 10 12/76 shotgun*\n[View all 12 Gauge ammo](https://i.imgur.com/pomaSEU.png)'
+    value: 'Budget Ammo: *12/70 8.5 mm "Magnum" Buckshot*\nBest Ammo: *12/70 AP-20 Slug*\nBest Weapon: *Saiga 12ga ver. 10 12/76 shotgun*\n[View all 12 Gauge ammo](https://i.imgur.com/fv31kZT.png)'
 }
 
 ammoTypes["20 Gauge"] = {
     name: '20 Gauge',
-    value: 'Budget Ammo: *20/70 5.6mm Buckshot*\nBest Ammo: *20/70 Slug Poleva-6u*\nBest Weapon: *TOZ-106 bolt-action shotgun*\n[View all 20 Gauge ammo](https://i.imgur.com/1tWatST.png)'
+    value: 'Budget Ammo: *20/70 5.6mm Buckshot*\nBest Ammo: *20/70 Slug Poleva-6u*\nBest Weapon: *TOZ-106 bolt-action shotgun*\n[View all 20 Gauge ammo](https://i.imgur.com/sNk6pS0.png)'
 }
 
 ammoTypes[".300"] = {
     name: '.300',
-    value: 'Budget Ammo: *.300 BPZ AAC Blackout*\nBest Ammo: *.300 AAC Blackout AP*\nBest Weapon: *SIG MCX .300 AAC Blackout Assault Rifle*\n[View all .300 ammo](https://i.imgur.com/fSHclne.png)'
+    value: 'Budget Ammo: *.300 BCP FMJ*\nBest Ammo: *.300 AP*\nBest Weapon: *SIG MCX .300 AAC Blackout Assault Rifle*\n[View all .300 ammo](https://i.imgur.com/schUNd3.png)'
 }
 
 ammoTypes[".338"] = {
     name: '.338',
-    value: 'Budget Ammo: *.338 UPZ Lapua Magnum*\nBest Ammo: *.338 Lapua Magnum AP*\nBest Weapon: *MK-18 .338 LM Marksman Rifle*\n[View all .338 ammo](https://i.imgur.com/1yZcohx.png)'
+    value: 'Budget Ammo: *.338 UPZ*\nBest Ammo: *.338 AP*\nBest Weapon: *MK-18 .338 LM Marksman Rifle*\n[View all .338 ammo](https://i.imgur.com/ohULLPQ.png)'
 }
 
 ammoTypes[".366"] = {
     name: '.366',
-    value: 'Budget Ammo: *.366 TKM EKO*\nBest Ammo: *.366 AP*\nBest Weapon: *Vepr AKM / VPO-209 366TKM carbine*\n[View all .366 ammo](https://i.imgur.com/XP5wUvl.png)'
+    value: 'Budget Ammo: *.366 TKM EKO*\nBest Ammo: *.366 AP*\nBest Weapon: *Vepr AKM / VPO-209 366TKM carbine*\n[View all .366 ammo](https://i.imgur.com/JoHLU3F.png)'
 }
 
-ammoTypes[".45 ACP"] = {
-    name: '.45 ACP',
-    value: 'Budget Ammo: *.45 ACP FMJ*\nBest Ammo: *.45 ACP AP*\nBest Weapon: *TDI KRISS Vector Gen.2 .45 ACP submachinegun*\n[View all .45 ACP ammo](https://i.imgur.com/WljXpwp.png)'
+ammoTypes[".45"] = {
+    name: '.45',
+    value: 'Budget Ammo: *.45 ACP*\nBest Ammo: *.45 ACP AP*\nBest Weapon: *TDI KRISS Vector Gen.2 .45 ACP submachinegun*\n[View all .45 ACP ammo](https://i.imgur.com/ViUMpiw.png)'
 }
 
 ammoTypes["4.6x30mm"] = {
     name: '4.6x30mm',
-    value: 'Budget Ammo: *4.6x30mm Subsonic SX*\nBest Ammo: *4.6x30mm AP SX*\nBest Weapon: *HK MP7A2 4.6x30 submachinegun*\n[View all 4.6x30mm ammo](https://i.imgur.com/oppsEKE.png)'
+    value: 'Budget Ammo: *4.6x30mm Subsonic SX*\nBest Ammo: *4.6x30mm AP SX*\nBest Weapon: *HK MP7A2 4.6x30 submachinegun*\n[View all 4.6x30mm ammo](https://i.imgur.com/xDk7kXn.png)'
 }
 
 ammoTypes["5.45x39mm"] = {
     name: '5.45x39mm',
-    value: 'Budget Ammo: *5.45x39 mm BT*\nBest Ammo: *5.45x39 mm BS*\nBest Weapon: *AK-74N*\n[View all 5.45x39mm ammo](https://i.imgur.com/Ew7apcX.png)'
+    value: 'Budget Ammo: *5.45x39 mm BP*\nBest Ammo: *5.45x39 mm PPBS "Igolnik"*\nBest Weapon: *AK-74N*\n[View all 5.45x39mm ammo](https://i.imgur.com/GUsTYez.png)'
 }
 
 ammoTypes["5.56x45mm"] = {
     name: '5.56x45mm',
-    value: 'Budget Ammo: *5.56x45 mm M856A1*\nBest Ammo: *5.56x45 mm M855A1*\nBest Weapon: *Colt M4A1 5.56x45 Assault Rifle*\n[View all 5.56x45mm ammo](https://i.imgur.com/khi6ZQh.png)'
+    value: 'Budget Ammo: *5.56x45 mm M856A1*\nBest Ammo: *5.56x45 mm M855A1*\nBest Weapon: *Colt M4A1 5.56x45 Assault Rifle*\n[View all 5.56x45mm ammo](https://i.imgur.com/tsWKo7o.png)'
 }
 
 ammoTypes["5.7x28mm"] = {
     name: '5.7x28mm',
-    value: 'Budget Ammo: *5.7x28 mm L191*\nBest Ammo: *5.7x28 mm SB193*\nBest Weapon: *FN P90 5.7x28 submachinegun*\n[View all 5.7x28mm ammo](https://i.imgur.com/6wXM9CH.png)'
+    value: 'Budget Ammo: *5.7x28 mm L191*\nBest Ammo: *5.7x28 mm SB193*\nBest Weapon: *FN P90 5.7x28 submachinegun*\n[View all 5.7x28mm ammo](https://i.imgur.com/TLVajdW.png)'
 }
 
 ammoTypes["7.62x25mm"] = {
     name: '7.62x25mm',
-    value: 'Best Ammo: *7.62x25mm TT Pst gzh*\nBest Weapon: *TT pistol 7.62x25 TT*\n[View all 7.62x25mm ammo](https://i.imgur.com/6Bx0XAM.png)'
+    value: 'Best Ammo: *7.62x25mm TT Pst gzh*\nBest Weapon: *TT pistol 7.62x25 TT*\n[View all 7.62x25mm ammo](https://i.imgur.com/NvfADnL.png)'
 }
 
 ammoTypes["7.62x39mm"] = {
     name: '7.62x39mm',
-    value: 'Budget Ammo: *7.62x39 mm PS*\nBest Ammo: *7.62x39 mm BP*\nBest Weapon: *AKM(N) 7.62x39 assault rifle*\n[View all 7.62x39mm ammo](https://i.imgur.com/VDz8i0V.png)'
+    value: 'Budget Ammo: *7.62x39 mm PS*\nBest Ammo: *7.62x39 mm BP*\nBest Weapon: *AKM(N) 7.62x39 assault rifle*\n[View all 7.62x39mm ammo](https://i.imgur.com/OJcOpgE.png)'
 }
 
 ammoTypes["7.62x51mm"] = {
     name: '7.62x51mm',
-    value: 'Budget Ammo: *7.62x51 mm M80*\nBest Ammo: *7.62x51 mm M61*\nBest Weapon: *Springfield Armory M1A 7.62x51*\n[View all 7.62x51mm ammo](https://i.imgur.com/MkYqg8g.png)'
+    value: 'Budget Ammo: *7.62x51 mm M80*\nBest Ammo: *7.62x51 mm M61*\nBest Weapon: *Springfield Armory M1A 7.62x51*\n[View all 7.62x51mm ammo](https://i.imgur.com/KKQjvgN.png)'
 }
 
 ammoTypes["7.62x54R"] = {
     name: '7.62x54R',
-    value: 'Budget Ammo: *7.62x54R LPS Gzh*\nBest Ammo: *7.62x54R 7N1 Sniper cartridge*\nBest Weapon: *SVDS 7.62x54 Sniper rifle*\n[View all 7.62x54R ammo](https://i.imgur.com/5331TPh.png)'
+    value: 'Budget Ammo: *7.62x54R LPS Gzh*\nBest Ammo: *7.62x54R BS*\nBest Weapon: *SVDS 7.62x54 Sniper rifle*\n[View all 7.62x54R ammo](https://i.imgur.com/mGbOim1.png)'
 }
 
 ammoTypes["9x18mm"] = {
     name: '9x18mm',
-    value: 'Budget Ammo: *9x18 mm PM SP7 gzh*\nBest Ammo: *9x18 mm PM PBM*\nBest Weapon: *PP-91 "Kedr"*\n[View all 9x18mm ammo](https://i.imgur.com/KjpdTkG.png)'
+    value: 'Budget Ammo: *9x18 mm PST gzh*\nBest Ammo: *9x18 mm PM PBM*\nBest Weapon: *PP-91 "Kedr"*\n[View all 9x18mm ammo](https://i.imgur.com/JAQOUmy.png)'
 }
 
 ammoTypes["9x19mm"] = {
     name: '9x19mm',
-    value: 'Budget Ammo: *9x19 mm Pst gzh*\nBest Ammo: *9x19 mm 7N31*\nBest Weapon: *SIG MPX 9x19 Submachine gun*\n[View all 9x19mm ammo](https://i.imgur.com/RQRJHT0.png)'
+    value: 'Budget Ammo: *9x19 mm Pst gzh*\nBest Ammo: *9x19 mm PBP*\nBest Weapon: *SIG MPX 9x19 Submachine gun*\n[View all 9x19mm ammo](https://i.imgur.com/a5x91Aw.png)'
 }
 
 ammoTypes["9x21mm"] = {
     name: '9x21mm',
-    value: 'Best Ammo: *9x21 mm SP13*\nBest Weapon: *9x21 Seryukov automatic pistol SR1MP Gyurza*\n[View all 9x21mm ammo](https://i.imgur.com/HnYN6XL.png)'
+    value: 'Best Ammo: *9x21 mm BT (Tracer)*\nBest Weapon: *9x21 Seryukov automatic pistol SR1MP Gyurza*\n[View all 9x21mm ammo](https://i.imgur.com/x8vfv16.png)'
 }
 
 ammoTypes["9x39mm"] = {
     name: '9x39mm',
-    value: 'Budget Ammo: *9x39 mm SP-5*\nBest Ammo: *9x39 mm BP*\nBest Weapon: *AS VAL*\n[View all 9x39mm ammo](https://i.imgur.com/wX41Jdl.png)'
+    value: 'Budget Ammo: *9x39 mm SP-5*\nBest Ammo: *9x39 mm BP*\nBest Weapon: *AS VAL*\n[View all 9x39mm ammo](https://i.imgur.com/njSHsgL.png)'
 }
 
 ammoTypes["12.7x55mm"] = {
     name: '12.7x55mm',
-    value: 'Budget Ammo: *12.7x55 mm PS12*\nBest Ammo: *12.7x55 mm PS12B*\nBest Weapon: *ASh-12 12.7x55 assault rifle*\n[View all 12.7x55mm ammo](https://i.imgur.com/900AGhI.png)'
+    value: 'Budget Ammo: *12.7x55 mm PS12*\nBest Ammo: *12.7x55 mm PS12B*\nBest Weapon: *ASh-12 12.7x55 assault rifle*\n[View all 12.7x55mm ammo](https://i.imgur.com/bvBsCNX.png)'
 }
 
 ammoTypes["23x75mm"] = {
     name: '23x75mm',
-    value: 'Budget Ammo: *Shrapnel 10*\nBest Ammo: *"Barricade" Slug*\nMisc Ammo: *"Star" Flashbang Slug*\nBest Weapon: *TOZ KS-23M 23x75mm shotgun*\n[View all 23x75mm ammo](https://i.imgur.com/wGzJ6za.png)'
+    value: 'Budget Ammo: *Shrapnel 10*\nBest Ammo: *"Barricade" Slug*\nMisc Ammo: *"Star" Flashbang Slug*\nBest Weapon: *TOZ KS-23M 23x75mm shotgun*\n[View all 23x75mm ammo](https://i.imgur.com/KaH7Fl2.png)'
 }
 
 function getHelp() {
@@ -129,7 +129,7 @@ ammoRegex["20 Gauge"] = /^(20(( |\-)?G((au|ua)ge)?)?|Shotgun|TOZ(\-| )?(106)?)$/
 ammoRegex[".300"] = /^(\.?300) ?(blk|blackout)?$/gi;
 ammoRegex[".338"] = /^(\.?338 ?(Lapua)? ?(Magnum)?)|(Lapua)|(Magnum)$/gi;
 ammoRegex[".366"] = /^(\.?366)( TKM)?$/gi;
-ammoRegex[".45 ACP"] = /^((\.?45 ?(ACP)?)|(ACP)|M1911(A1)?)$/gi;
+ammoRegex[".45"] = /^((\.?45 ?(ACP)?)|(ACP)|M1911(A1)?)$/gi;
 ammoRegex["4.6x30mm"] = /^(4\.?6(x30)?(mm)?|MP7)$/gi;
 ammoRegex["5.45x39mm"] = /^(5\.?45(x39)?(mm)?|AKS?\-?(74(M|N|UB?)?|10[125])?)|RPK|5\.45$/gi;
 ammoRegex["5.56x45mm"] = /^(5\.?56(x45)?(mm)?)|M4((\-| )?A1)|HK((\-| )?416(A5)?)?|DT(\-| )?(MDR)?|MDR|TX(\-15)? ?(DML)?|5\.56$/gi;
