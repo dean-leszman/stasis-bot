148ae6a761755e7852e9410a1d1bde44956c30b6
Dean Leszman <dean.leszman@gmail.com>
2021-10-19 14:44:33 +0100

    Added image command

---
+++ src/commands/util/image.js
@@ -0,0 +1,32 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class ImageCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'image',
+            group: 'util',
+            memberName: 'image',
+            description: 'Post an image',
+            throttling: {
+                usages: 1,
+                duration: 3
+            },
+            args: [{
+                key: 'url',
+                prompt: 'Plase enter an image URL',
+                type: 'string',
+                default: ''
+            }]
+        });
+    }
+
+    run(message, { url }) {
+        message.say("", {
+            files: [url]
+        })
+        .then(() => {
+            message.delete()
+            .catch(console.error);
+        });
+    }
+}
