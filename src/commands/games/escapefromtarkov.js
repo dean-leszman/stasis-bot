beef56000680c4964b5c95e2cc06bd57a4458f3c
Dean Leszman <dean.leszman@gmail.com>
2021-11-01 11:51:47 +0000

    EFT case insensitive ammo and maps

--- src/commands/games/escapefromtarkov.js
+++ src/commands/games/escapefromtarkov.js
@@ -3,12 +3,12 @@ const { MessageEmbed } = require('discord.js');
 
 const ammoTypes = [];
 
-ammoTypes["12 Gauge"] = {
+ammoTypes["12 GAUGE"] = {
     name: '12 Gauge',
     value: 'Budget Ammo: *12/70 8.5 mm "Magnum" Buckshot*\nBest Ammo: *12/70 AP-20 Slug*\nBest Weapon: *Saiga 12ga ver. 10 12/76 shotgun*\n[View all 12 Gauge ammo](https://i.imgur.com/fv31kZT.png)'
 }
 
-ammoTypes["20 Gauge"] = {
+ammoTypes["20 GAUGE"] = {
     name: '20 Gauge',
     value: 'Budget Ammo: *20/70 5.6mm Buckshot*\nBest Ammo: *20/70 Slug Poleva-6u*\nBest Weapon: *TOZ-106 bolt-action shotgun*\n[View all 20 Gauge ammo](https://i.imgur.com/sNk6pS0.png)'
 }
@@ -33,37 +33,37 @@ ammoTypes[".45"] = {
     value: 'Budget Ammo: *.45 ACP*\nBest Ammo: *.45 ACP AP*\nBest Weapon: *TDI KRISS Vector Gen.2 .45 ACP submachinegun*\n[View all .45 ACP ammo](https://i.imgur.com/ViUMpiw.png)'
 }
 
-ammoTypes["4.6x30mm"] = {
+ammoTypes["4.6x30MM"] = {
     name: '4.6x30mm',
     value: 'Budget Ammo: *4.6x30mm Subsonic SX*\nBest Ammo: *4.6x30mm AP SX*\nBest Weapon: *HK MP7A2 4.6x30 submachinegun*\n[View all 4.6x30mm ammo](https://i.imgur.com/xDk7kXn.png)'
 }
 
-ammoTypes["5.45x39mm"] = {
+ammoTypes["5.45x39MM"] = {
     name: '5.45x39mm',
     value: 'Budget Ammo: *5.45x39 mm BP*\nBest Ammo: *5.45x39 mm PPBS "Igolnik"*\nBest Weapon: *AK-74N*\n[View all 5.45x39mm ammo](https://i.imgur.com/GUsTYez.png)'
 }
 
-ammoTypes["5.56x45mm"] = {
+ammoTypes["5.56x45MM"] = {
     name: '5.56x45mm',
     value: 'Budget Ammo: *5.56x45 mm M856A1*\nBest Ammo: *5.56x45 mm M855A1*\nBest Weapon: *Colt M4A1 5.56x45 Assault Rifle*\n[View all 5.56x45mm ammo](https://i.imgur.com/tsWKo7o.png)'
 }
 
-ammoTypes["5.7x28mm"] = {
+ammoTypes["5.7x28MM"] = {
     name: '5.7x28mm',
     value: 'Budget Ammo: *5.7x28 mm L191*\nBest Ammo: *5.7x28 mm SB193*\nBest Weapon: *FN P90 5.7x28 submachinegun*\n[View all 5.7x28mm ammo](https://i.imgur.com/TLVajdW.png)'
 }
 
-ammoTypes["7.62x25mm"] = {
+ammoTypes["7.62x25MM"] = {
     name: '7.62x25mm',
     value: 'Best Ammo: *7.62x25mm TT Pst gzh*\nBest Weapon: *TT pistol 7.62x25 TT*\n[View all 7.62x25mm ammo](https://i.imgur.com/NvfADnL.png)'
 }
 
-ammoTypes["7.62x39mm"] = {
+ammoTypes["7.62x39MM"] = {
     name: '7.62x39mm',
     value: 'Budget Ammo: *7.62x39 mm PS*\nBest Ammo: *7.62x39 mm BP*\nBest Weapon: *AKM(N) 7.62x39 assault rifle*\n[View all 7.62x39mm ammo](https://i.imgur.com/OJcOpgE.png)'
 }
 
-ammoTypes["7.62x51mm"] = {
+ammoTypes["7.62x51MM"] = {
     name: '7.62x51mm',
     value: 'Budget Ammo: *7.62x51 mm M80*\nBest Ammo: *7.62x51 mm M61*\nBest Weapon: *Springfield Armory M1A 7.62x51*\n[View all 7.62x51mm ammo](https://i.imgur.com/KKQjvgN.png)'
 }
@@ -73,32 +73,32 @@ ammoTypes["7.62x54R"] = {
     value: 'Budget Ammo: *7.62x54R LPS Gzh*\nBest Ammo: *7.62x54R BS*\nBest Weapon: *SVDS 7.62x54 Sniper rifle*\n[View all 7.62x54R ammo](https://i.imgur.com/mGbOim1.png)'
 }
 
-ammoTypes["9x18mm"] = {
+ammoTypes["9x18MM"] = {
     name: '9x18mm',
     value: 'Budget Ammo: *9x18 mm PST gzh*\nBest Ammo: *9x18 mm PM PBM*\nBest Weapon: *PP-91 "Kedr"*\n[View all 9x18mm ammo](https://i.imgur.com/JAQOUmy.png)'
 }
 
-ammoTypes["9x19mm"] = {
+ammoTypes["9x19MM"] = {
     name: '9x19mm',
     value: 'Budget Ammo: *9x19 mm Pst gzh*\nBest Ammo: *9x19 mm PBP*\nBest Weapon: *SIG MPX 9x19 Submachine gun*\n[View all 9x19mm ammo](https://i.imgur.com/a5x91Aw.png)'
 }
 
-ammoTypes["9x21mm"] = {
+ammoTypes["9x21MM"] = {
     name: '9x21mm',
     value: 'Best Ammo: *9x21 mm BT (Tracer)*\nBest Weapon: *9x21 Seryukov automatic pistol SR1MP Gyurza*\n[View all 9x21mm ammo](https://i.imgur.com/x8vfv16.png)'
 }
 
-ammoTypes["9x39mm"] = {
+ammoTypes["9x39MM"] = {
     name: '9x39mm',
     value: 'Budget Ammo: *9x39 mm SP-5*\nBest Ammo: *9x39 mm BP*\nBest Weapon: *AS VAL*\n[View all 9x39mm ammo](https://i.imgur.com/njSHsgL.png)'
 }
 
-ammoTypes["12.7x55mm"] = {
+ammoTypes["12.7x55MM"] = {
     name: '12.7x55mm',
     value: 'Budget Ammo: *12.7x55 mm PS12*\nBest Ammo: *12.7x55 mm PS12B*\nBest Weapon: *ASh-12 12.7x55 assault rifle*\n[View all 12.7x55mm ammo](https://i.imgur.com/bvBsCNX.png)'
 }
 
-ammoTypes["23x75mm"] = {
+ammoTypes["23x75MM"] = {
     name: '23x75mm',
     value: 'Budget Ammo: *Shrapnel 10*\nBest Ammo: *"Barricade" Slug*\nMisc Ammo: *"Star" Flashbang Slug*\nBest Weapon: *TOZ KS-23M 23x75mm shotgun*\n[View all 23x75mm ammo](https://i.imgur.com/KaH7Fl2.png)'
 }
@@ -157,7 +157,6 @@ function getAllAmmoTypes() {
 
 function getMap(map) {
     let response;
-    if (map) map = map.toUpperCase();
 
     switch(map) {
         case "CUSTOMS":
@@ -253,7 +252,7 @@ module.exports = class TarkovCommand extends Command {
     run(message, { category, type }) {
         switch (category.toUpperCase()) {
             case "AMMO":
-                if (type) return message.embed(getAmmoByType(type));
+                if (type) return message.embed(getAmmoByType(type.toUpperCase()));
                 return message.embed(getAllAmmoTypes());
             case "CUSTOMS":
             case "FACTORY":
@@ -264,10 +263,10 @@ module.exports = class TarkovCommand extends Command {
             case "RESERVE":
             case "RESORT":
             case "WOODS":
-                return message.say(getMap(category));
+                return message.say(getMap(category.toUpperCase()));
             case "MAPS":
             case "MAP":
-                return message.say(getMap(type));
+                return message.say(getMap(type.toUpperCase()));
             case "KEYS":
                 return message.say("https://i.imgur.com/7UbjEPq.png");
             case "HELP":
