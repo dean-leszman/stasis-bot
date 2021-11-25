38eb7e0c324bc546e69d42b988909d60678219e8
Dean Leszman <dean.leszman@gmail.com>
2021-11-25 16:58:26 +0000

    Added help command

---
+++ src/commands/help.js
@@ -0,0 +1,24 @@
+const { MessageEmbed } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('View a list of commands supported by Stasis Bot.'),
+    async execute(interaction) {
+        let description = '';
+        interaction.client.commands.each(command => {
+            if (!command.wip) {
+                description += `\`/${command.data.name}\` - ${command.data.description}\n`;
+            }
+        });
+
+        const embed = new MessageEmbed()
+            .setTitle('Stasis Bot Commands')
+            .setDescription(description);
+
+        interaction.reply({
+            embeds: [embed]
+        });
+    }
+};
