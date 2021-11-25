62e0c5ec15f6ce45d14338cd076a4177767b32dc
Dean Leszman <dean.leszman@gmail.com>
2021-11-25 11:56:25 +0000

    Added Escape From Tarkov command

---
+++ src/data/EFT.js
@@ -0,0 +1,118 @@
+module.exports = {
+    'AMMO': {
+        '12_GAUGE': {
+            name: '12 Gauge',
+            description: 'Budget Ammo: *12/70 8.5 mm "Magnum" Buckshot*\nBest Ammo: *12/70 AP-20 Slug*\nBest Weapon: *Saiga 12ga ver. 10 12/76 shotgun*\n[View all 12 Gauge ammo](https://i.imgur.com/fv31kZT.png)'
+        },
+        '20_GAUGE': {
+            name: '20 Gauge',
+            description: 'Budget Ammo: *20/70 5.6mm Buckshot*\nBest Ammo: *20/70 Slug Poleva-6u*\nBest Weapon: *TOZ-106 bolt-action shotgun*\n[View all 20 Gauge ammo](https://i.imgur.com/sNk6pS0.png)'
+        },
+        '.300': {
+            name: '.300',
+            description: 'Budget Ammo: *.300 BCP FMJ*\nBest Ammo: *.300 AP*\nBest Weapon: *SIG MCX .300 AAC Blackout Assault Rifle*\n[View all .300 ammo](https://i.imgur.com/schUNd3.png)'
+        },
+        '.338': {
+            name: '.338',
+            description: 'Budget Ammo: *.338 UPZ*\nBest Ammo: *.338 AP*\nBest Weapon: *MK-18 .338 LM Marksman Rifle*\n[View all .338 ammo](https://i.imgur.com/ohULLPQ.png)'
+        },
+        '.366': {
+            name: '.366',
+            description: 'Budget Ammo: *.366 TKM EKO*\nBest Ammo: *.366 AP*\nBest Weapon: *Vepr AKM / VPO-209 366TKM carbine*\n[View all .366 ammo](https://i.imgur.com/JoHLU3F.png)'
+        },
+        '.45': {
+            name: '.45',
+            description: 'Budget Ammo: *.45 ACP*\nBest Ammo: *.45 ACP AP*\nBest Weapon: *TDI KRISS Vector Gen.2 .45 ACP submachinegun*\n[View all .45 ACP ammo](https://i.imgur.com/ViUMpiw.png)'
+        },
+        '4.6X30MM': {
+            name: '4.6x30mm',
+            description: 'Budget Ammo: *4.6x30mm Subsonic SX*\nBest Ammo: *4.6x30mm AP SX*\nBest Weapon: *HK MP7A2 4.6x30 submachinegun*\n[View all 4.6x30mm ammo](https://i.imgur.com/xDk7kXn.png)'
+        },
+        '5.45X39MM': {
+            name: '5.45x39mm',
+            description: 'Budget Ammo: *5.45x39 mm BP*\nBest Ammo: *5.45x39 mm PPBS "Igolnik"*\nBest Weapon: *AK-74N*\n[View all 5.45x39mm ammo](https://i.imgur.com/GUsTYez.png)'
+        },
+        '5.56X45MM': {
+            name: '5.56x45mm',
+            description: 'Budget Ammo: *5.56x45 mm M856A1*\nBest Ammo: *5.56x45 mm M855A1*\nBest Weapon: *Colt M4A1 5.56x45 Assault Rifle*\n[View all 5.56x45mm ammo](https://i.imgur.com/tsWKo7o.png)'
+        },
+        '5.7X28MM': {
+            name: '5.7x28mm',
+            description: 'Budget Ammo: *5.7x28 mm L191*\nBest Ammo: *5.7x28 mm SB193*\nBest Weapon: *FN P90 5.7x28 submachinegun*\n[View all 5.7x28mm ammo](https://i.imgur.com/TLVajdW.png)'
+        },
+        '7.62X25MM': {
+            name: '7.62x25mm',
+            description: 'Best Ammo: *7.62x25mm TT Pst gzh*\nBest Weapon: *TT pistol 7.62x25 TT*\n[View all 7.62x25mm ammo](https://i.imgur.com/NvfADnL.png)'
+        },
+        '7.62X39MM': {
+            name: '7.62x39mm',
+            description: 'Budget Ammo: *7.62x39 mm PS*\nBest Ammo: *7.62x39 mm BP*\nBest Weapon: *AKM(N) 7.62x39 assault rifle*\n[View all 7.62x39mm ammo](https://i.imgur.com/OJcOpgE.png)'
+        },
+        '7.62X51MM': {
+            name: '7.62x51mm',
+            description: 'Budget Ammo: *7.62x51 mm M80*\nBest Ammo: *7.62x51 mm M61*\nBest Weapon: *Springfield Armory M1A 7.62x51*\n[View all 7.62x51mm ammo](https://i.imgur.com/KKQjvgN.png)'
+        },
+        '7.62X54R': {
+            name: '7.62x54R',
+            description: 'Budget Ammo: *7.62x54R LPS Gzh*\nBest Ammo: *7.62x54R BS*\nBest Weapon: *SVDS 7.62x54 Sniper rifle*\n[View all 7.62x54R ammo](https://i.imgur.com/mGbOim1.png)'
+        },
+        '9X18MM': {
+            name: '9x18mm',
+            description: 'Budget Ammo: *9x18 mm PST gzh*\nBest Ammo: *9x18 mm PM PBM*\nBest Weapon: *PP-91 "Kedr"*\n[View all 9x18mm ammo](https://i.imgur.com/JAQOUmy.png)'
+        },
+        '9X19MM': {
+            name: '9x19mm',
+            description: 'Budget Ammo: *9x19 mm Pst gzh*\nBest Ammo: *9x19 mm PBP*\nBest Weapon: *SIG MPX 9x19 Submachine gun*\n[View all 9x19mm ammo](https://i.imgur.com/a5x91Aw.png)'
+        },
+        '9X21MM': {
+            name: '9x21mm',
+            description: 'Best Ammo: *9x21 mm BT (Tracer)*\nBest Weapon: *9x21 Seryukov automatic pistol SR1MP Gyurza*\n[View all 9x21mm ammo](https://i.imgur.com/x8vfv16.png)'
+        },
+        '9X39MM': {
+            name: '9x39mm',
+            description: 'Budget Ammo: *9x39 mm SP-5*\nBest Ammo: *9x39 mm BP*\nBest Weapon: *AS VAL*\n[View all 9x39mm ammo](https://i.imgur.com/njSHsgL.png)'
+        },
+        '12.7X55MM': {
+            name: '12.7x55mm',
+            description: 'Budget Ammo: *12.7x55 mm PS12*\nBest Ammo: *12.7x55 mm PS12B*\nBest Weapon: *ASh-12 12.7x55 assault rifle*\n[View all 12.7x55mm ammo](https://i.imgur.com/bvBsCNX.png)'
+        },
+        '23X75MM': {
+            name: '23x75mm',
+            description: 'Budget Ammo: *Shrapnel 10*\nBest Ammo: *"Barricade" Slug*\nMisc Ammo: *"Star" Flashbang Slug*\nBest Weapon: *TOZ KS-23M 23x75mm shotgun*\n[View all 23x75mm ammo](https://i.imgur.com/KaH7Fl2.png)'
+        }
+    },
+    'MAPS': {
+        'CUSTOMS': {
+            name: 'Customs',
+            url: 'https://i.imgur.com/AAZhPrj.png'
+        },
+        'FACTORY': {
+            name: 'Factory',
+            url: 'https://i.imgur.com/yYRHXD9.jpg'
+        },
+        'INTERCHANGE': {
+            name: 'Interchange',
+            url: 'https://i.imgur.com/aUXxB3j.jpg'
+        },
+        'LABS': {
+            name: 'Labs',
+            url: 'https://www.gamemaps.co.uk/game/tarkov/maps/lab_explained_en'
+        },
+        'RESERVE': {
+            name: 'Reserve',
+            url: 'https://i.imgur.com/ExJTzEU.jpg'
+        },
+        'SHORELINE': {
+            name: 'Shoreline',
+            url: 'https://i.imgur.com/OjSqUzp.jpg'
+        },
+        'SHORELINE_RESORT': {
+            name: 'Shoreline Resort',
+            url: 'https://i.imgur.com/2AhMmEq.jpeg'
+        },
+        'WOODS': {
+            name: 'Woods',
+            url: 'https://i.imgur.com/QH6STpH.png'
+        }
+    }
+}
