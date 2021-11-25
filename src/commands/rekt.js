1faba220575e4250e64fd35c9ba66f51bdce9c94
Dean Leszman <dean.leszman@gmail.com>
2021-11-25 16:58:15 +0000

    Updated rekt description

--- src/commands/rekt.js
+++ src/commands/rekt.js
@@ -4,7 +4,7 @@ const phrases = require('../data/Rekt');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('rekt')
-        .setDescription('Get rekt kiddo'),
+        .setDescription('Get rekt kiddo.'),
     async execute(interaction) {
         const phrase = phrases[Math.floor(Math.random() * phrases.length)];
         interaction.reply({
