73426c0d613ac95adea258865908dbb2061c24b9
Dean Leszman <dean.leszman@gmail.com>
2021-12-09 12:53:25 +0000

    feat: Added github command

---
+++ src/commands/github.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('github')
+        .setDescription('Get a link to the bot\'s Github repository.'),
+    async execute(interaction) {
+        await interaction.reply({
+            content: 'https://github.com/dean-leszman/stasis-bot',
+            ephemeral: true
+        });
+    }
+};
