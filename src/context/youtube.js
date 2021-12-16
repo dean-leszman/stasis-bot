a6a887bc878e23e39c841bdd494862f38ec440c5
Dean Leszman <dean.leszman@gmail.com>
2021-12-16 17:31:45 +0000

    feat: Added YouTube context menu search

---
+++ src/context/youtube.js
@@ -0,0 +1,15 @@
+const { ApplicationCommandType } = require('discord-api-types/v9');
+const { ContextMenuCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new ContextMenuCommandBuilder()
+        .setName("YouTube Search")
+        .setType(ApplicationCommandType.Message),
+    async execute(interaction) {
+        const message = interaction.options.getMessage('message');
+
+        await interaction.reply({
+            content: `https://www.youtube.com/results?search_query=${encodeURIComponent(message.content)}`
+        });
+    }
+}
