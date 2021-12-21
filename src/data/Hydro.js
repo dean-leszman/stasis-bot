3479c50254d53b8fd3014a11b7cc90e487b23823
Dean Leszman <dean.leszman@gmail.com>
2021-12-21 14:10:13 +0000

    feat: [Hydro] Added randomised images for drink and thirsty

---
+++ src/data/Hydro.js
@@ -0,0 +1,18 @@
+module.exports = {
+    drink: [
+        "https://i.imgur.com/Pni9HPP.gif",
+        "https://i.imgur.com/B9Xtv8a.gif",
+        "https://i.imgur.com/KYBHYdU.mp4",
+        "https://i.imgur.com/EUFNKmM.gif",
+        "https://i.imgur.com/HmKYkpq.gif",
+        "https://i.imgur.com/FafYSkN.gif"
+    ],
+    thirsty: [
+        "https://i.imgur.com/oACR1rm.gif",
+        "https://i.imgur.com/XGwEBOn.gif",
+        "https://i.imgur.com/BAcghP7.gif",
+        "https://i.imgur.com/7K6GWyH.gif",
+        "https://i.imgur.com/lFqVrMt.gif",
+        "https://i.imgur.com/RH7RGqv.gif"
+    ]
+};
