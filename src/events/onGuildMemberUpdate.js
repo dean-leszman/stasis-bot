62f43159c5d575ccf7fe8ce5e5790ae7d66258b7
Dean Leszman <github@deanleszman.com>
2022-01-26 15:23:13 +0000

    feat - added name change logs

---
+++ src/events/onGuildMemberUpdate.js
@@ -0,0 +1,21 @@
+const { MessageEmbed } = require('discord.js');
+const { SERVER: config } = require('../data/Config');
+const { COLORS: colors } = require('../data/Static');
+
+module.exports = {
+    name: 'guildMemberUpdate',
+    async execute(oldMember, member) {
+        const logChannel = member.guild.channels.cache.find(channel => channel.id === config.logChannel);
+        if (oldMember.displayName !== member.displayName) {
+            const embed = new MessageEmbed()
+                .setTitle("__Name Changed__")
+                .setColor(colors.orange)
+                .addField("Old", oldMember.displayName, true)
+                .addField("New", member.displayName);
+
+            logChannel.send({
+                embeds: [embed]
+            });
+        }
+    }
+}
