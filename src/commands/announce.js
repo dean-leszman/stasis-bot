95766851196c96e49170454be346bf562e8314f5
Dean Leszman <github@deanleszman.com>
2022-05-17 13:08:32 +0100

    feat: Announce command

---
+++ src/commands/announce.js
@@ -0,0 +1,43 @@
+const { MessageEmbed, Permissions } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { colors } = require('../data/Static');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('announce')
+        .setDescription('Create an announcement')
+        .addStringOption(option => 
+            option.setName('title')
+            .setDescription('Announcement title')
+            .setRequired(true)
+        )
+        .addStringOption(option => 
+            option.setName('message')
+            .setDescription('Announcement message')
+            .setRequired(true)
+        )
+        .addStringOption(option =>
+            option.setName('author')
+            .setDescription('Author name. (Default: Council of Ministers)')
+        ),
+    async execute(interaction) {
+        const title = interaction.options.getString('title');
+        const message = interaction.options.getString('message');
+
+        const embed = new MessageEmbed()
+            .setTitle(title)
+            .setDescription(message)
+            .setAuthor("Council of Ministers", interaction.guild.iconURL())
+            .setColor(colors.teal);
+
+        const author = interaction.options.getString('author') ?? "Council of Ministers";
+        embed.setAuthor(author, interaction.guild.iconURL());
+
+        interaction.reply({
+            embeds: [embed]
+        });
+    },
+    permissions: {
+        command: [Permissions.FLAGS.ADMINISTRATOR]
+    }
+}
