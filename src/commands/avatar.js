c63e0d49dcab056931db249aa7bf27d372b3f6e3
Dean Leszman <github@deanleszman.com>
2022-05-18 10:07:26 +0100

    feat: avatar command

---
+++ src/commands/avatar.js
@@ -0,0 +1,50 @@
+const { MessageAttachment } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+function getGuildAvatar(interaction) {
+    return interaction.guild.iconURL();
+}
+
+function getMemberAvatar(interaction) {
+    const member = interaction.options.getMember('user');
+    return member.displayAvatarURL();
+}
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('avatar')
+        .setDescription('Get an avatar')
+        .addSubcommand(cmd => 
+            cmd.setName('user')
+            .setDescription('Get a user\'s avatar ')
+            .addUserOption(option =>
+                option.setName('user')
+                .setDescription('The user you want to get the avatar from')
+                .setRequired(true)
+            )
+        )
+        .addSubcommand(cmd =>
+            cmd.setName('server')
+            .setDescription('Get the server\'s avatar')
+        ),
+    async execute(interaction) {
+        let avatar;
+
+        switch (interaction.options.getSubcommand()) {
+            case 'server': {
+                avatar = getGuildAvatar(interaction);
+                break;
+            }
+
+            case 'user': {
+                avatar = getMemberAvatar(interaction);
+                break;
+            }
+        }
+
+        image = new MessageAttachment(avatar);
+        interaction.reply({
+            files: [image]
+        });
+    }
+}
