53afa3318a368f3aa91f0ad2916fefa693a787e9
Dean Leszman <github@deanleszman.com>
2023-04-04 00:28:57 +0100

    Path of Exile 3.21 Atlas Maps and Builds

--- src/data/PathOfExile.js
+++ src/data/PathOfExile.js
@@ -1,142 +1,151 @@
 module.exports = {
     atlas: {
-        version: "3.20",
+        version: "3.21",
         tiers: {
             1: [
-                "Lava Lake",
-                "Mausoleum",
-                "Strand",
-                "Terrace"
+                "Bone Crypt",
+                "Scriptorium",
+                "Alleyways",
+                "Colonnade"
             ],
             2: [
-                "Alleyways",
-                "Arachnid Tomb",
-                "Cage",
-                "Castle Ruins",
-                "Crimson Temple",
-                "Forking River",
-                "Moon Temple",
-                "Spider Forest"
+                "Dunes",
+                "Stagnation",
+                "Silo",
+                "Dry Sea",
+                "Excavation",
+                "Belfry",
+                "Precinct",
+                "Grotto"
             ],
-            3: [
-                "Bone Crypt",
-                "Caldera",
-                "Coves",
-                "Museum",
-                "Phantasmagoria",
-                "Plateau",
-                "Port",
-                "Sepulchre"
+                3: [
+                "Overgrown Shrine",
+                "Strand",
+                "Lair",
+                "Infested Valley",
+                "Shrine",
+                "Primordial Blocks",
+                "Primordial Pool",
+                "Fields"
             ],
             4: [
-                "Arachnid Nest",
-                "Atoll",
-                "Coral Ruins",
-                "Residence",
-                "Shore",
-                "Spider Lair",
-                "Thicket",
-                "Vaal Pyramid"
+                "Cemetery",
+                "Moon Temple",
+                "Vaal Pyramid",
+                "Lighthouse",
+                "Orchard",
+                "Laboratory",
+                "Port",
+                "Tower"
             ],
             5: [
-                "Acid Caverns",
-                "Bog",
+                "Atoll",
                 "Courtyard",
-                "Ivory Temple",
-                "Laboratory",
-                "Pier",
-                "Silo",
-                "Temple"
+                "City Square",
+                "Marshes",
+                "Mineral Pools",
+                "Plateau",
+                "Arcade",
+                "Residence"
             ],
             6: [
-                "Haunted Mansion",
-                "Iceberg",
-                "Overgrown Shrine",
-                "Peninsula",
+                "Shore",
                 "Promenade",
-                "Sulphur Vents",
-                "Volcano"
+                "Pit",
+                "Phantasmagoria",
+                "Castle Ruins",
+                "Waste Pool",
+                "Toxic Sewer"
             ],
             7: [
-                "Burial Chambers",
-                "Crater",
-                "Factory",
-                "Frozen Cabins",
-                "Mineral Pools",
-                "Necropolis",
-                "Precinct"
+                "Cursed Crypt",
+                "Canyon",
+                "Sulphur Vents",
+                "Relic Chambers",
+                "Chateau",
+                "Coral Ruins",
+                "Forbidden Woods"
             ],
             8: [
-                "Academy",
-                "Arsenal",
-                "Crimson Township",
-                "Dungeon",
-                "Estuary",
-                "Leyline",
-                "Primordial Blocks"
+                "Crimson Temple",
+                "Shipyard",
+                "Caldera",
+                "Armoury",
+                "Arachnid Tomb",
+                "Flooded Mine",
+                "Crystal Ore"
             ],
             9: [
-                "Arcade",
-                "Bazaar",
-                "Colosseum",
-                "Dark Forest",
-                "Desert",
-                "Primordial Pool"
+                "Wasteland",
+                "Ramparts",
+                "Pier",
+                "Overgrown Ruin",
+                "Cold River",
+                "Siege"
             ],
             10: [
-                "Defiled Cathedral",
-                "Forbidden Woods",
-                "Gardens",
-                "Grave Trough",
-                "Lookout",
-                "Underground River"
+                "Underground Sea",
+                "Foundry",
+                "Arachnid Nest",
+                "Cells",
+                "Factory",
+                "Plaza"
             ],
             11: [
-                "City Square",
-                "Dry Sea",
-                "Infested Valley",
-                "Mesa",
-                "Villa",
-                "Wasteland"
+                "Malformation",
+                "Fungal Hollow",
+                "Beach",
+                "Geode",
+                "Crimson Township",
+                "Frozen Cabins"
             ],
             12: [
-                "Cemetery",
-                "Cold River",
-                "Colonnade",
-                "Courthouse",
-                "Cursed Crypt",
-                "Shipyard"
+                "Museum",
+                "Necropolis",
+                "Desert Spring",
+                "Conservatory",
+                "Jungle Valley",
+                "Wharf"
             ],
             13: [
-                "Channel",
-                "Lair",
-                "Orchard",
-                "Plaza",
-                "Shrine"
+                "Arid Lake",
+                "Leyline",
+                "Defiled Cathedral",
+                "Crater",
+                "Thicket"
             ],
             14: [
-                "Canyon",
-                "Carcass",
+                "Temple",
                 "Maze",
-                "Sunken City",
-                "Underground Sea"
+                "Barrows",
+                "Dungeon",
+                "Terrace"
             ],
             15: [
-                "Ashen Wood",
-                "Dunes",
-                "Foundry",
-                "Grotto",
-                "Overgrown Ruin"
+                "Underground River",
+                "Tropical Island",
+                "Colosseum",
+                "Core",
+                "Racecourse"
             ],
             16: [
-                "Ancient City",
-                "Ghetto",
-                "Graveyard",
-                "Toxic Sewer"
+                "Dig",
+                "Volcano",
+                "Coves",
+                "Arsenal"
             ]
         }
     },
     builds: [{
+        ascendancy: "Ascendant",
+        builds: [{
+            name: "[3.21] Exsanguinate",
+            link: "https://www.pathofexile.com/forum/view-thread/3324458"
+        }, {
+            name: "[3.21] Holy Flame Totem (Chieftain + Heirophant)",
+            link: "https://www.pathofexile.com/forum/view-thread/3146104"
+        }]
+    }, {
         ascendancy: "Champion",
         builds: [{
             name: "[3.20] Explosive Arrow",
@@ -145,20 +154,32 @@ module.exports = {
     }, {
         ascendancy: "Deadeye",
         builds: [{
+            name: "[3.21] Tornado Shot (Ice Shot / Lightning Arrow)",
+            link: "https://www.pathofexile.com/forum/view-thread/3359691"
+        }, {
             name: "[3.19] Venom Gyre",
             link: "https://www.youtube.com/watch?v=UNrg63Kltgo"
         }]
     }, {
         ascendancy: "Elementalist",
         builds: [{
-            name: "[3.20] Explosive Arrow Ballista",
-            link: "https://www.youtube.com/watch?v=grWNV7YLvIc"
+            name: "[3.21] Arc",
+            link: "https://www.pathofexile.com/forum/view-thread/1147951"
         }, {
-            name: "[3.19] Lightning Conduit",
-            link: "https://www.youtube.com/watch?v=GGnpVhm-8dU"
+            name: "[3.21] Cold DoT",
+            link: "https://www.youtube.com/watch?v=WHAUGGhJY4w"
+        }, {
+            name: "[3.21] Explosive Arrow Ballista",
+            link: "https://www.pathofexile.com/forum/view-thread/3229979"
+        }, { 
+            name: "[3.21] Fire Trap",
+            link: "https://www.youtube.com/watch?v=xsViMao5-ug"
         }, {
             name: "[3.20] Wave of Conviction Ignite",
             link: "https://www.youtube.com/watch?v=nqxl8UsO5Cc"
+        }, {
+            name: "[3.19] Lightning Conduit",
+            link: "https://www.youtube.com/watch?v=GGnpVhm-8dU"
         }]
     }, {
         ascendancy: "Gladiator",
@@ -173,32 +194,65 @@ module.exports = {
             link: "https://www.youtube.com/watch?v=EntDk7PVxVs"
         }]
     }, {
+        ascendancy: "Heirophant",
+        builds: [{
+            name: "[3.21] Arc Mana Stacker",
+            link: "https://www.youtube.com/watch?v=1QsZ5pRbzg8"
+        }, {
+            name: "[3.21] MoM Crit Power Siphon Totems",
+            link: "https://www.pathofexile.com/forum/view-thread/3317015"
+        }]
+    }, { 
+        ascendancy: "Guardian",
+        builds: [{
+            name: "[3.21] Spider + Herald of Purity",
+            link: "https://www.pathofexile.com/forum/view-thread/2554248"
+        }]
+    }, {
         ascendancy: "Inquisitor",
         builds: [{
+            name: "[3.21] Galvanic Arc",
+            link: "https://www.pathofexile.com/forum/view-thread/3246656"
+        }, {
             name: "[3.19] Righteous Fire",
             link: "https://www.poe-vault.com/guides/righteous-fire-inquisitor-build-guide"
         }]
     }, {
         ascendancy: "Juggernaut",
         builds: [{
-            name: "[3.20] Righteous Fire",
-            link: "https://pastebin.com/BSSQpce8"
+            name: "[3.21] Boneshatter",
+            link: "https://www.youtube.com/watch?v=5EJ2nc34lkg"
+        }, {
+            name: "[3.21] Righteous Fire",
+            link: "https://www.youtube.com/watch?v=Ji0CqE8F3wU"
         }]
     }, {
         ascendancy: "Necromancer",
         builds: [{
-            name: "[3.19] Absolution",
-            link: "https://www.youtube.com/watch?v=-OMiuuJTT6E"
+            name: "[3.21] Dark Pact",
+            link: "https://www.youtube.com/watch?v=5pShdfiBxU0"
+        }, {
+            name: "[3.21] Flicker Strike",
+            link: "https://www.pathofexile.com/forum/view-thread/3002453"
+        }, {
+            name: "[3.21] Summon Raging Skeletons",
+            link: "https://www.pathofexile.com/forum/view-thread/2995833"
         }, {
             name: "[3.20] Dark Pact",
             link: "https://www.youtube.com/watch?v=XtO34CMuOkU"
         }, {
             name: "[3.20] Summoner (Skeletons + Raging Spirits)",
             link: "https://www.youtube.com/watch?v=EntDk7PVxVs"
+        }, {
+            name: "[3.19] Absolution",
+            link: "https://www.youtube.com/watch?v=-OMiuuJTT6E"
         }]
     }, {
         ascendancy: "Occultist",
         builds: [{
+            name: "[3.21] Death's Oath Occultist",
+            link: "https://www.pathofexile.com/forum/view-thread/3293805"
+        }, {
             name: "[3.20] Bane",
             link: "https://www.youtube.com/watch?v=OwkA7KYCDNo"
         }, {
@@ -211,15 +265,27 @@ module.exports = {
     }, {
         ascendancy: "Pathfinder",
         builds: [{
+            name: "[3.21] Toxic Rain",
+            link: "https://www.youtube.com/watch?v=W20G6bZJqR4"
+        }, {
+            name: "[3.21] Toxic Rain Ballista",
+            link: "https://www.youtube.com/watch?v=KAcrchacF8g"
+        }, {
+            name: "[3.21] Poison Tornado Shot",
+            link: "https://www.youtube.com/watch?v=-5WlT5y6ON4"
+        }, {
             name: "[3.20] Poisonous Concoction",
             link: "https://www.youtube.com/watch?v=eSEIDoasiwg"
-        }, {
-            name: "[3.20] Toxic Rain",
-            link: "https://www.youtube.com/watch?v=5RB_v5akxOY"
         }]
     }, {
         ascendancy: "Raider",
         builds: [{
+            name: "[3.21] Cold Flicker Strike",
+            link: "https://www.youtube.com/watch?v=mZ8UFGFUM1U"
+        }, {
+            name: "[3.21] Lightning Arrow + Artillery Ballista",
+            link: "https://www.youtube.com/watch?v=uq2xxUlTcwc"
+        }, {
             name: "[3.19] Spectral Helix",
             link: "https://www.youtube.com/watch?v=Vv3tjc7Mzdk"
         }]
@@ -228,6 +294,12 @@ module.exports = {
         builds: [{
             name: "[3.20] Seismic Trap",
             link: "https://www.youtube.com/watch?v=IRC9wSHSVKs"
+        }]
+    }, {
+        ascendancy: "Slayer",
+        builds: [{
+            name: "[3.21] Shield Crush",
+            link: "https://www.pathofexile.com/forum/view-thread/3148278"
         }]
     }, {
         ascendancy: "Trickster",
