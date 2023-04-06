458bf1d53da6239639c4cc150630bbfed6485457
Dean Leszman <github@deanleszman.com>
2023-04-06 11:55:22 +0100

    ref: remove role command

--- src/events/onGuildMemberAdd.js
+++ src/events/onGuildMemberAdd.js
@@ -1,9 +1,6 @@
-const { joinRoles } = require('../data/Roles');
-
 module.exports = {
     name: 'guildMemberAdd',
     async execute(guildMember) {
         console.log(`${guildMember.displayName} has joined the server.`)
-        guildMember.roles.set(joinRoles, "Join roles");
     }
 };
